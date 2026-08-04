365caeab5d93b50516108b8c7aa1e54c25dc9f29
fabioboschel-lang <fabioboschel@gmail.com>
2026-08-04 19:41:07 -0300

    Add Supabase client initialization in supabase.js

---
+++ supabase.js
@@ -0,0 +1,20 @@
+```javascript
+import {
+  createClient
+} from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
+
+
+const supabaseUrl =
+  "https://qexgbswdbwlpydolpcll.supabase.co";
+
+
+const supabaseKey =
+  "PEGÁ_ACÁ_TU_PUBLISHABLE_KEY";
+
+
+export const supabase =
+  createClient(
+    supabaseUrl,
+    supabaseKey
+  );
+```
