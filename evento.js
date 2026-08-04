9e2962ed12014827bdf656ad28c01ab4d6b7b1d5
fabioboschel-lang <fabioboschel@gmail.com>
2026-08-04 18:32:51 -0300

    Create evento.js

---
+++ evento.js
@@ -0,0 +1,23 @@
+export function Evento(app) {
+
+  app.innerHTML = `
+
+    <main class="evento-view">
+
+      <section class="evento-card">
+
+        <h1>
+          Evento Vybe
+        </h1>
+
+        <p>
+          La página del evento se cargará aquí.
+        </p>
+
+      </section>
+
+    </main>
+
+  `;
+
+}
