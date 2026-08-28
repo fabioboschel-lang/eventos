ef8a4b8fa7309a06d9cad25faf582ab986c00f9a
fabioboschel-lang <fabioboschel@gmail.com>
2026-08-28 12:12:31 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+import { aplicarPlanilla1 } from "./planilla1.js";
+import { aplicarPlanilla2 } from "./planilla2.js";
+import { aplicarPlanilla3 } from "./planilla3.js";
+import { aplicarPlanilla4 } from "./planilla4.js";
+
+
+export const planillas = {
+
+  1: aplicarPlanilla1,
+
+  2: aplicarPlanilla2,
+
+  3: aplicarPlanilla3,
+
+  4: aplicarPlanilla4
+
+};
