89cf5baed7fcf123b9e3517e4f7864f4ed008c12
fabioboschel-lang <fabioboschel@gmail.com>
2026-09-03 12:58:55 -0300

    Actualizar supabase.js

--- supabase.js
+++ supabase.js
@@ -1,16 +1,10 @@
 
-import {
-  createClient
-} from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
+const supabaseUrl = "https://qexgbswdbwlpydolpcll.supabase.co"
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFleGdic3dkYndscHlkb2xwY2xsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMxMjM4OTIsImV4cCI6MjA4ODY5OTg5Mn0.9VuIgA28S47iBKacaKMA8plq82uoP_1XcCAUI1znhBQ"
 
-const supabaseUrl =
-  "https://qexgbswdbwlpydolpcll.supabase.co";
-
-
-const supabaseKey =
-  "sb_publishable_3Vo6VOuDzVbN5355c9HeDA_1YwQPR6l";
-
+export const supabase = createClient(supabaseUrl, supabaseKey)
 
 export const supabase =
   createClient(
