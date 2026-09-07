f803ac4e11729a790c19834bb3f3cd527e7afad3
fabioboschel-lang <fabioboschel@gmail.com>
2026-09-07 08:39:01 -0300

    Add files via upload

---
+++ autenticacionhtml.js
@@ -0,0 +1,28 @@
+/*
+
+==========================================================
+
+HTML AUTENTICACIÓN
+
+==========================================================
+*/
+
+
+export const HTMLautenticacion = `
+
+<section
+  id="autenticacion"
+  class="autenticacion"
+>
+
+  <button
+    id="googleBtn"
+    class="google-btn"
+    type="button"
+  >
+    Continuar con Google
+  </button>
+
+</section>
+
+`;
---
+++ google.js
@@ -0,0 +1,124 @@
+import { supabase } from "./supabase.js";
+
+
+/*
+
+==========================================================
+
+INICIALIZAR GOOGLE
+
+==========================================================
+*/
+
+
+export function inicializarGoogle() {
+
+
+  const googleBtn =
+    document.getElementById(
+      "googleBtn"
+    );
+
+
+  if (!googleBtn) {
+
+    console.error(
+      "No existe el botón de Google."
+    );
+
+    return;
+
+  }
+
+
+  /*
+
+  ========================================================
+
+  LISTENER GOOGLE
+
+  ========================================================
+  */
+
+
+  googleBtn.addEventListener(
+    "click",
+    async () => {
+
+
+      /*
+
+      ======================================================
+
+      OBTENER URL ACTUAL
+
+      ======================================================
+      */
+
+
+      const redirectTo =
+        window.location.origin +
+        window.location.pathname +
+        window.location.hash;
+
+
+      /*
+
+      ======================================================
+
+      AUTENTICACIÓN CON GOOGLE
+
+      ======================================================
+      */
+
+
+      const {
+        data,
+        error
+      } =
+        await supabase.auth.signInWithOAuth({
+
+          provider:
+            "google",
+
+          options: {
+
+            redirectTo:
+              redirectTo
+
+          }
+
+        });
+
+
+      console.log(
+        data
+      );
+
+
+      /*
+
+      ======================================================
+
+      ERROR
+
+      ======================================================
+      */
+
+
+      if (error) {
+
+        console.error(
+          error
+        );
+
+        alert(
+          "No se pudo iniciar sesión."
+        );
+
+      }
+
+    }
+  );
+
+}
